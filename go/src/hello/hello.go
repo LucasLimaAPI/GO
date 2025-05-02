a037d5be08d2667b9786636faa9a4ea3de3b1d70
Lucas Lima de Oliveira <96830935+LucasLimaAPI@users.noreply.github.com>
2025-05-02 20:29:57 -0300

    GoLang
    Aprendendo os primeiros passos na GoLang

---
+++ go/src/hello/hello.go
@@ -0,0 +1,7 @@
+package main // Sempre será necessário importar o pacote
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World/Olá Mundo") //Importa a funcionalidade e usa ela novamente dando o nome da function
+}
