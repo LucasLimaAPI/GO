3a4d7198bef05ed372b2b541ca4e7e712237364d
Lucas Lima de Oliveira <96830935+LucasLimaAPI@users.noreply.github.com>
2025-05-02 21:47:41 -0300

    Adicionando variaveis

--- go/src/hello/hello.go
+++ go/src/hello/hello.go
@@ -1,7 +1,14 @@
 package main // Sempre será necessário importar o pacote
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World/Olá Mundo") //Importa a funcionalidade e usa ela novamente dando o nome da function
+	name := "Lucas" //ao invés de usarmos var podemos usar ":="
+	version := 1.1
+	old := 23
+
+	fmt.Println("Hello Mr.", name, "Are You:", old, "Years old") //Importa a funcionalidade e usa ela novamente dando o nome da function
+	fmt.Println("This Program Version is:", version)
 }
